7f32b23a625405afe95f5a4eb632bbd106f8ec4d
Vis <gonnavis@163.com>
2016-02-18 23:20:47 +0800

    more accurate date

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ angular.module('timeline',[])
 	$s.zoom=0.3;
 	$s.periodHeight=30;
 
-	// var timeline={
+	// var timeline={//wiki
 	// 	'夏朝':{from:-2100,to:-1600},
 	// 	'商朝':{from:-1600,to:-1050},
 	// 	'周朝':{from:-1046,to:-256},
@@ -22,23 +22,48 @@ angular.module('timeline',[])
 	// 	'清朝':{from:1644,to:1912}
 	// }
 
-	var timeline={//sjudo
+	var timeline={//https://www.awesomestories.com/images/user/b4cbb7aae7.gif
 		data:[
-			{name:'商朝',from:-1600,to:-1050},
-			{name:'秦朝',from:-1100,to:-228},
-			{name:'唐朝',from:618,to:906},
+			{name:'夏朝',from:-2000,to:-1500},
+			{name:'商朝',from:-1700,to:-1027},
+			{name:'周朝',from:-1066,to:-221},
+			{name:'秦朝',from:-221,to:-207},
 			{name:'汉朝',from:-206,to:220},
-			{name:'夏朝',from:-2100,to:-1500},
-			{name:'周朝',from:-1846,to:-256},
+			{name:'三国',from:220,to:280},
 			{name:'晋朝',from:265,to:420},
-			{name:'隋朝',from:581,to:638},
-			{name:'元朝',from:1100,to:1368},
-			{name:'明朝',from:1390,to:1700},
-			{name:'清朝',from:1644,to:1912},
-			{name:'宋朝',from:800,to:1279},
-			{name:'aaa',from:-300,to:300}
+			{name:'南朝',from:420,to:588},
+			{name:'北朝',from:386,to:588},
+			{name:'隋朝',from:581,to:617},
+			{name:'唐朝',from:618,to:907},
+			{name:'五代',from:907,to:960},
+			{name:'十国',from:907,to:979},
+			{name:'宋朝',from:960,to:1279},
+			{name:'辽',from:916,to:1225},
+			{name:'西夏',from:1038,to:1227},
+			{name:'金',from:1115,to:1234},
+			{name:'元朝',from:1279,to:1368},
+			{name:'明朝',from:1368,to:1644},
+			{name:'清朝',from:1644,to:1911}
 		]
 	}
+
+	// var timeline={//pseudo
+	// 	data:[
+	// 		{name:'商朝',from:-1600,to:-1050},
+	// 		{name:'秦朝',from:-1100,to:-228},
+	// 		{name:'唐朝',from:618,to:906},
+	// 		{name:'汉朝',from:-206,to:220},
+	// 		{name:'夏朝',from:-2100,to:-1500},
+	// 		{name:'周朝',from:-1846,to:-256},
+	// 		{name:'晋朝',from:265,to:420},
+	// 		{name:'隋朝',from:581,to:638},
+	// 		{name:'元朝',from:1100,to:1368},
+	// 		{name:'明朝',from:1390,to:1700},
+	// 		{name:'清朝',from:1644,to:1912},
+	// 		{name:'宋朝',from:800,to:1279},
+	// 		{name:'aaa',from:-300,to:300}
+	// 	]
+	// }
 
 	timeline.data.sort(function(a,b){return a.from-b.from});//排序
 
