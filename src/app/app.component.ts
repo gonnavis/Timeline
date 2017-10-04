a5c4c8203e00e5605ef8c23445cc3d629f57e441
gonnavis <gonnavis@163.com>
2017-10-05 01:58:05 +0800

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TimelineService } from './timeline.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  periodHeight=30;
+  zoom=0.3;
+  bias=0;
+  totalWidth=0;
+  min;
+  zoomStep=0.1;
+  zoomMin=0.1;
+  zoomFix=1;
+  constructor(private timelineService: TimelineService) { }
+  ngOnInit() {
+    console.log(this.timelineService.data);
+    console.log(this.timelineService.rowData);
+  }
+  test(){
+  }
+  areas=[1]
+  rows=this.timelineService.rowData
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import{TimelineService}from'./timeline.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TimelineService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
