4c1ce249c760ff9707dfa7d615b02c9d1e540d80
gonnavis <gonnavis@163.com>
2018-10-09 14:43:44 +0800

    a

--- src/components/preprocess_data.js
+++ src/components/preprocess_data.js
@@ -3,5 +3,11 @@ import areas from './data'
 
 timeline_service.processTimelines(areas)
 var global={areas}
+global.min=areas[0].min
+areas.forEach((area, i)=>{
+  if(area.min<global.min){
+    global.min=area.min;
+  }
+})
 
 export default global
--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="component timeline">
-    <div class="areas">
-      <div v-for="area in global.areas">
-        <div v-for="row in area.rows">
-          <div v-for="period in row.periods">{{period.name}}</div>
+    <div class="global" :style="get_global_style()">
+      <div class="area" v-for="(area, ai) in global.areas" :style="get_area_style(area, ai)">
+        <div class="row" v-for="row in area.rows">
+          <div class="period" v-for="period in row.periods">{{period.name}}</div>
         </div>
       </div>
     </div>
@@ -19,7 +19,8 @@ export default {
     return {
       r:null,
       global:global,
-      period_height:100,
+      period_height:30,
+      scale:.1,
     }
   },
   created(){
@@ -32,6 +33,28 @@ export default {
 
   },
   methods:{
+    get_global_style(){
+      let s=this;
+      let style={
+        position: 'absolute',
+        top: '0px',
+        left: -s.global.min*s.scale+'px',
+      }
+      return style;
+    },
+    get_area_style(area, ai){
+      let s=this
+      let height=area.rows.length*s.period_height
+      let style={
+        position: 'absolute',
+        height: height+'px',
+        width: area.span*s.scale+'px',
+        left: area.min*s.scale+'px',
+        top: ai*height+'px',
+        'background-color': `rgb(${255*Math.random()}, ${255*Math.random()}, ${255*Math.random()})`,
+      };
+      return style;
+    }
   }
 }
 </script>
