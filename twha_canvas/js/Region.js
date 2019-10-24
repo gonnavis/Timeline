a594c324a523c36a62bebc546bf0fa6bf5cd48d9
gonnavis <gonnavis@163.com>
2019-10-24 14:48:23 +0800

    twha_canvas init

---
+++ twha_canvas/js/Region.js
@@ -0,0 +1,157 @@
+'use strict';
+
+var BOX_WIDTH = 160;
+var REGION_WIDTH = BOX_WIDTH + 48 + 10;
+
+function Region(a)
+{
+	var region_name = [null, null, null];
+	var region_abbr = [null, null, null];
+	var person_list = [];
+	var flag = null;
+
+	this.pos_x = 0;
+	this.pos_y = 0;
+	this.disp_level = 0;
+
+	function create_region_box()
+	{
+		var node = document.createElement('div');
+		node.classList.add('region');
+
+		var title = document.createElement('div');
+		title.classList.add('region-title');
+		node.appendChild(title);
+
+		return node;
+	}
+
+	this.node = create_region_box();
+
+
+	function lang_name_to_id(lang)
+	{
+		switch (lang) {
+		case 'ja':
+			return 0;
+		case 'en':
+			return 1;
+		case 'zh':
+			return 2;
+		}
+	}
+	function set_default_name(a, offset)
+	{
+		if (a[offset + 0] === '$') {
+			a[offset + 0] = a[offset + 1];
+		}
+		if (a[offset + 1] === '@') {
+			a[offset + 1] = a[offset + 0];
+		}
+		if (a[offset + 2] === '$') {
+			a[offset + 2] = a[offset + 1];
+		}
+		if (a[offset + 2] === '@') {
+			a[offset + 2] = a[offset + 0];
+		}
+	}
+
+	this.update_year = function()
+	{
+		var year = data.year;
+		var i, b, title = null;
+
+		// 国名
+		for (i = 3; i < a.length && a[i].length > 3; i++) {
+			b = a[i];
+			flag = b[2];
+			if (b[3]) {
+				set_default_name(b, 3);
+				region_name[0] = b[3];
+				region_name[1] = b[4];
+				region_name[2] = b[5];
+			}
+			if (b.length >= 9 && b[6]) {
+				set_default_name(b, 6);
+				region_abbr[0] = b[6];
+				region_abbr[1] = b[7];
+				region_abbr[2] = b[8];
+			}
+			if (b.length >= 12) {
+				this.pos_x = b[9];
+				this.pos_y = b[10];
+				this.disp_level = b[11];
+			}
+			if (year < b[1]) {
+				break;
+			}
+		}
+		for (; i < a.length && a[i].length > 3; i++) {
+		}
+		// 人名
+		person_list = [];
+		for (; i < a.length; i++) {
+			b = a[i];
+			if (b.length == 3) {
+				set_default_name(b, 0);
+				title = b;
+			} else {
+				if (year >= b[0] && year < b[1]) {
+					set_default_name(b, 3);
+					person_list.push([title, b]);
+				}
+			}
+		}
+		this.node.style.zIndex = this.pos_y - this.disp_level * 100 + 400;
+	};
+	this.update = function(x, y)
+	{
+		this.node.style.left = x + 'px';
+		this.node.style.top = (y - 8) + 'px';
+
+		var lang = lang_name_to_id(data.lang);
+		var html = '';
+		if (flag && data.zoom >= 1){
+			html = '<img src="../twha/sym/' + flag + '.png" alt="">';
+		}
+		if (data.zoom - this.disp_level >= 2) {
+			html += region_name[lang];
+		} else {
+			html += region_abbr[lang];
+		}
+		var n = this.node.childNodes[0];
+		n.innerHTML = html;
+
+		if (data.zoom - this.disp_level >= 2) {
+			var body, item;
+			if (this.node.childNodes.length == 2) {
+				body = this.node.childNodes[1];
+				body.innerHTML = '';
+			} else {
+				body = document.createElement('div');
+				body.classList.add('person-list');
+				this.node.appendChild(body);
+			}
+			for (var i = 0; i < person_list.length; i++) {
+				var a_title = person_list[i][0];
+				var a_person = person_list[i][1];
+				html = '';
+
+				item = document.createElement('div');
+				item.classList.add('person');
+				if (a_person[2]) {
+					html = '<img src="../twha/f/' + a_person[2] + '.png" alt="">';
+				} else {
+					html = '<img src="../twha/f/0.png" alt="">';
+				}
+				html += '<div>' + a_title[lang] + '</div><div>' + a_person[3 + lang] + '</div>';
+				item.innerHTML = html;
+				body.appendChild(item);
+			}
+		} else {
+			if (this.node.childNodes.length == 2) {
+				this.node.removeChild(this.node.childNodes[1]);
+			}
+		}
+	};
+}
---
+++ twha_canvas/js/YearBar.js
@@ -0,0 +1,153 @@
+'use strict';
+
+function YearBar(glob)
+{
+	var _SIZE = 32;
+
+	var year_bar = document.getElementById('year-bar');
+	var arrow_l = document.getElementById('year-arrow-left');
+	var arrow_r = document.getElementById('year-arrow-right');
+	var scale = document.getElementById('year-bar-scale');
+	var cursor = document.getElementById('year-bar-cursor');
+	var scale_width = 1;
+	var on_changed_handler = null;
+	this.SIZE = _SIZE;
+
+
+	this.set_top = function(top)
+	{
+		year_bar.style.top = top + 'px';
+	};
+	this.set_width = function(width)
+	{
+		if (width < 1) {
+			width = 1;
+		}
+		scale_width = width;
+		scale.setAttribute('width', width);
+
+		var ctx = scale.getContext('2d');
+		ctx.clearRect(0, 0, width, _SIZE);
+		ctx.fillStyle = '#e0e0e0'
+		ctx.fillRect(0, 0, width, _SIZE);
+
+		var pattern = [
+			0, 0,
+			1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+			1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+			1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+			1, 1,
+		];
+		var length = pattern.length;
+		var i;
+		ctx.fillStyle = '#c0c0c0'
+		for (i = 0; i < length; i++) {
+			if (pattern[i]) {
+				var x1 = width * i / length;
+				var x2 = width * (i + 1) / length;
+				ctx.fillRect(x1, 0, x2 - x1, _SIZE);
+			}
+		}
+		ctx.fillStyle = 'black';
+		ctx.font = '9px sans-serif'
+		ctx.textAlign = 'center';
+		ctx.textBaseline = 'middle';
+		for (i = 0; i < 4; i++) {
+			ctx.fillText(String(-4000 + i * 1000), (2 + i * 10) * width / length, _SIZE / 2);
+		}
+		for (i = 0; i < 6; i++) {
+			ctx.fillText(String(-500 + i * 500), (42 + i * 10) * width / length, _SIZE / 2);
+		}
+
+		update_cursor();
+	};
+	function update_cursor()
+	{
+		data.year_clamp();
+
+		var yr = data.year + 4000;
+		if (yr > 3000) {
+			yr = yr * 2 - 3000;
+		}
+		cursor.style.left = ((yr + 200) * scale_width / 9400 + 26) + 'px';
+	}
+
+	this.onchanged = function(f)
+	{
+		on_changed_handler = f;
+	};
+	this.update = update_cursor;
+
+  year_bar.addEventListener('contextmenu', function(e){ e.preventDefault() })
+  year_bar.addEventListener('mousewheel', function(e)
+  {
+    console.log(e)
+    e.preventDefault()
+    e.stopPropagation()
+    var step
+    if(e.ctrlKey || e.buttons===2) step=10
+    else if(e.shiftKey) step=5
+    else step=1
+    if(e.deltaY>0){
+      data.year+=step
+    }else if(e.deltaY<0){
+      data.year-=step
+    }
+    update_cursor();
+    if (on_changed_handler) {
+      on_changed_handler();
+    }
+  });
+
+  var is_mousedown=false
+  year_bar.addEventListener('mousedown', function(e){
+    // console.log(e)
+    glob.pan_target=year_bar
+    set_year(e)
+  })
+	window.addEventListener('mousemove', function(e)
+	{
+    if(!glob.pan_target || glob.pan_target!==year_bar){
+      return
+    }
+    e.preventDefault()
+    set_year(e)
+	});
+	arrow_l.addEventListener('mouseenter', function()
+	{
+		arrow_l.src = '../twha/img/arrow-left2.png';
+	});
+	arrow_l.addEventListener('mouseleave', function()
+	{
+		arrow_l.src = '../twha/img/arrow-left.png';
+	});
+	arrow_r.addEventListener('mouseenter', function()
+	{
+		arrow_r.src = '../twha/img/arrow-right2.png';
+	});
+	arrow_r.addEventListener('mouseleave', function()
+	{
+		arrow_r.src = '../twha/img/arrow-right.png';
+	});
+
+  function set_year(e){
+    if(e.buttons!==1) return
+    var xpos = e.clientX;
+    if (xpos < _SIZE) {
+      data.year--;
+    } else if (xpos > scale_width + _SIZE) {
+      data.year++;
+    } else {
+      var yr = (xpos - 32) * 9400 / scale_width - 200;
+      if (yr > 3000) {
+        yr = (yr + 3000) / 2;
+      }
+      yr -= 4000;
+      data.year = Math.round(yr);
+    }
+    update_cursor();
+    if (on_changed_handler) {
+      on_changed_handler();
+    }
+  }
+}
