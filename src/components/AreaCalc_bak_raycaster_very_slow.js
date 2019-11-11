be357a0e570aa274173811caebdf518430266726
gonnavis <gonnavis@163.com>
2019-11-11 19:13:03 +0800

    a

--- src/components/AreaCalc_bak_raycaster_very_slow.js
+++ src/components/AreaCalc_bak_raycaster_very_slow.js
@@ -4,7 +4,6 @@ class AreaCalc {
   raycaster = new THREE.Raycaster();
   group = new THREE.Group()
   count = 0
-  imageDatas=[]
 
   // geo = new THREE.IcosahedronBufferGeometry(.05, 1);
   // mtl = new THREE.MeshBasicMaterial({ color: "black" });
@@ -20,19 +19,17 @@ class AreaCalc {
     let s = this
     const origin = s.get_random_sphere_position(11)
     const direction = new THREE.Vector3().sub(origin).normalize()
-    //todo 自己写球面映射, 不要用raycaster
-    // s.raycaster.set(origin, direction);
-    // const intersects = s.raycaster.intersectObject(s.arg.mesh);
+    s.raycaster.set(origin, direction);
+    const intersects = s.raycaster.intersectObject(s.arg.mesh);
     // console.log(intersects)
-    // const intersect = intersects[0]
-    // window.intersect = intersect //todo
-    // if (intersect) {
-      const imageData = s.get_color(ctx_regions, new THREE.Vector2(Math.random(),Math.random()))
+    const intersect = intersects[0]
+    window.intersect = intersect //todo
+    if (intersect) {
+      const imageData = s.get_color(ctx_regions, intersect.uv)
       // console.log(imageData.data)
       if (imageData.data[0] === 255 && imageData.data[1] === 0 && imageData.data[2] === 0) {
 
-        s.count++
-        s.imageDatas.push(imageData)
+        count++
 
         // let obj3d = new THREE.Mesh(s.geo, s.mtl);
         // obj3d.position.copy(intersect.point)
@@ -43,7 +40,7 @@ class AreaCalc {
         // obj3d.position.copy(intersect.point)
         // this.group.add(obj3d)
       }
-    // }
+    }
   }
   get_random_sphere_position(radius) {
     let position = new THREE.Vector3(Math.random() - .5, Math.random() - .5, Math.random() - .5, )
