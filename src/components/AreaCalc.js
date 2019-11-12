324887637d279a16b3b70b5fe286c62ac9e33125
gonnavis <gonnavis@163.com>
2019-11-12 13:55:11 +0800

    color calc ok

--- src/components/AreaCalc.js
+++ src/components/AreaCalc.js
@@ -1,10 +1,11 @@
 import * as THREE from '../lib/three.module.js';
+import { getUvFromSpherePosition } from './getSpherePositionFromUv.js'
 
 class AreaCalc {
   raycaster = new THREE.Raycaster();
   group = new THREE.Group()
   count = 0
-  imageDatas=[]
+  imageDatas = []
 
   // geo = new THREE.IcosahedronBufferGeometry(.05, 1);
   // mtl = new THREE.MeshBasicMaterial({ color: "black" });
@@ -18,8 +19,8 @@ class AreaCalc {
   }
   add_dot() {
     let s = this
-    const origin = s.get_random_sphere_position(11)
-    const direction = new THREE.Vector3().sub(origin).normalize()
+    const origin = s.get_random_sphere_position(10)
+    // const direction = new THREE.Vector3().sub(origin).normalize()
     //todo 自己写球面映射, 不要用raycaster
     // s.raycaster.set(origin, direction);
     // const intersects = s.raycaster.intersectObject(s.arg.mesh);
@@ -27,22 +28,24 @@ class AreaCalc {
     // const intersect = intersects[0]
     // window.intersect = intersect //todo
     // if (intersect) {
-      const imageData = s.get_color(ctx_regions, new THREE.Vector2(Math.random(),Math.random()))
-      // console.log(imageData.data)
-      if (imageData.data[0] === 255 && imageData.data[1] === 0 && imageData.data[2] === 0) {
+    const uv = getUvFromSpherePosition(origin)
+    // console.log(uv)
+    const imageData = s.get_color(ctx_regions, uv)
+    // console.log(imageData.data)
+    if (imageData.data[0] === 255 && imageData.data[1] === 153 && imageData.data[2] === 153) {
 
-        s.count++
-        s.imageDatas.push(imageData)
+      s.count++
+      // s.imageDatas.push(imageData)
 
-        // let obj3d = new THREE.Mesh(s.geo, s.mtl);
-        // obj3d.position.copy(intersect.point)
+      // let obj3d = new THREE.Mesh(s.geo, s.mtl);
+      // obj3d.position.copy(intersect.point)
 
-        // let obj3d = new THREE.Sprite(s.mtl);
-        // let scale = .005
-        // obj3d.scale.set(scale, scale, scale, )
-        // obj3d.position.copy(intersect.point)
-        // this.group.add(obj3d)
-      }
+      let obj3d = new THREE.Sprite(s.mtl);
+      let scale = .005
+      obj3d.scale.set(scale, scale, scale, )
+      obj3d.position.copy(origin)
+      this.group.add(obj3d)
+    }
     // }
   }
   get_random_sphere_position(radius) {
@@ -59,7 +62,7 @@ export default AreaCalc
 
 /*//console use
   for(let i=0;i<1000;i++){
-    console.log(i)
     area_clac.add_dot()
   }
+  console.log('ok')
 */
--- src/components/getSpherePositionFromUv.js
+++ src/components/getSpherePositionFromUv.js
@@ -1,9 +1,59 @@
-var getSpherePositionFromUv = function(u, v, radius = 1) {
+import * as THREE from "../lib/three.module.js";
+
+const x_axis = new THREE.Vector3(1, 0, 0)
+const y_axis = new THREE.Vector3(0, 1, 0)
+const z_axis = new THREE.Vector3(0, 0, 1)
+const nx_axis = new THREE.Vector3(-1, 0, 0)
+const ny_axis = new THREE.Vector3(0, -1, 0)
+const nz_axis = new THREE.Vector3(0, 0, -1)
+const PI = Math.PI
+const PI2 = Math.PI * 2
+// const ref_axis= z_axis.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), (Math.PI / 180) * -101)
+
+function getSpherePositionFromUv(u, v, radius = 1) {
   let position = new THREE.Vector3(0, -radius, 0)
   position.applyAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI * v)
-  position.applyAxisAngle(new THREE.Vector3(0, 1, 0), (Math.PI / 180)*-101 + Math.PI * 2 * u)
+  position.applyAxisAngle(new THREE.Vector3(0, 1, 0), (Math.PI / 180) * -101 + Math.PI * 2 * u)
   return position
 };
 
+window.getUvFromSpherePosition = getUvFromSpherePosition //todo
+function getUvFromSpherePosition(position_in) {
+  const position = position_in.clone()
 
-export { getSpherePositionFromUv }
+  const xz_part = new THREE.Vector3(position.x, 0, position.z)
+  let angle_y = xz_part.angleTo(z_axis)
+  if (position.x < 0) angle_y = PI2 - angle_y
+  let u = angle_y / PI2
+
+  position.applyAxisAngle(new THREE.Vector3(0, 1, 0), angle_y)
+  // const yz_part = new THREE.Vector3(0, position.y, position.z)
+  const angle_x = position.angleTo(ny_axis)
+  let v = angle_x / PI
+  // if (position.z < 0) v = 1 - v
+  // u = u - 101 / 360
+  // if (u < 0) u = 1 + u
+  u = u - .2185
+  if (u < 0) {
+    u = 1 + u
+  }
+  const uv = new THREE.Vector2(u, v)
+  return uv
+}
+
+/*//todo
+  getUvFromSpherePosition(new THREE.Vector3(1,0,0))
+  three.module.js?b6b2:2174 THREE.Vector3: angleTo() can't handle zero length vectors.
+*/
+
+
+export { getSpherePositionFromUv, getUvFromSpherePosition }
+
+/*//console test
+  getUvFromSpherePosition(new THREE.Vector3(1,0,1))
+    expect {x: 0.125, y: 0.5}
+  getUvFromSpherePosition(new THREE.Vector3(-1,0,1))
+    expect {x: 0.875, y: 0.5}
+  getUvFromSpherePosition(new THREE.Vector3(-1,0,-1))
+    expect {x: 0.625, y: 0.5}
+*/
