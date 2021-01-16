f38b2d069e6d8f3ad98e47aa92236856f31dd22f
gonnavis <gonnavis@163.com>
2021-01-16 23:30:26 +0800

    fix: style

--- src/components/App.vue
+++ src/components/App.vue
@@ -26,7 +26,7 @@ export default {
 </script>
 
 <style>
-html,body{position: fixed;left: 0;top: 0;width:100%;height: 100%;overflow: hidden;margin:0;}
+html,body{position: fixed;left: 0;top: 0;width:100%;height: 100%;overflow: hidden !important;margin:0;}
 #app {font-family: "Avenir", Helvetica, Arial, sans-serif; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; text-align: center; color: #2c3e50; margin-top: 60px; position: absolute;left: 0;top: 0;width:100%;height: 100%;overflow: hidden;margin: 0;padding: 0; font-size: 14px;}
 .clearfix::after {content: ""; clear: both; display: table; }
 .full{position: absolute;left:0;top:0;width: 100%;height: 100%;}
